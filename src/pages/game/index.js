711710e2bd04c324de198fe8e1723efab918a3fb
Marco Bruno <marco.bruno.br@gmail.com>
2019-01-26 00:41:26 +0000

    Antes de fazer o código da live

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -2,26 +2,35 @@ const $root = document.querySelector("#root");
 
 const $cardsWrapper = createCardsWrapper();
 
+const $memoryCard = createMemoryCard({
+  src: "img/icon-collabcode.png",
+  alt: "Ícone do mascote Gueio da CollabCode"
+});
 const $memoryCardC = createMemoryCard({
   nameClass: "-front",
   src: "img/icon-c.png",
   alt: "Ícone de um livro da linguagem C++"
 });
+
 const $memoryCardJS = createMemoryCard({
   src: "img/icon-js.png",
   nameClass: "-front",
   alt: "Ícone de um livro da linguagem JavaScript"
 });
+
 const $memoryCardJava = createMemoryCard({
   nameClass: "-front",
   src: "img/icon-java.png",
   alt: "Ícone de um livro da linguagem Java"
 });
+
 const $memoryCardWoman = createMemoryCard({
   nameClass: "-front",
   src: "img/icon-woman.png",
   alt: "Ícone de um mina codando"
 });
+
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
 
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJava);
