ea63129338aef969a367605324ba6a6910b99391
Marco Bruno <marco.bruno.br@gmail.com>
2019-02-23 20:15:35 +0000

    035-06-sumir-com-o-botao-e-a-camada

--- src/components/game-button/index.js
+++ src/components/game-button/index.js
@@ -28,13 +28,16 @@ const gameButton = (function() {
     $head.insertBefore($style, null);
   };
 
-  module.handleClick = () => console.log("FUNCIONOU!!!")
+  module.handleClick = $component => {
+    $component.remove();
+    document.querySelector(".transparency-layer").remove();
+  };
 
   module.render = () => {
     module._style();
 
     return `
-      <button class="game-button" onClick="gameButton.handleClick()">Start</button>
+      <button class="game-button" onClick="gameButton.handleClick(this)">Start</button>
     `;
   };
 
