90f9c1b36bae1fcb1ac3fc15ea78eb8cd2fef856
Marco Bruno <marco.bruno.br@gmail.com>
2019-03-04 21:43:12 +0000

    Fim da Aula 38

--- src/components/flat-button/index.js
+++ src/components/flat-button/index.js
@@ -11,7 +11,6 @@ const flatButton = (function() {
         color: #fffcee;
         font-size: 24px;
         font-weight: bold;
-        border: none;
         width: 186px;
         height: 176px;
         text-transform: uppercase;
