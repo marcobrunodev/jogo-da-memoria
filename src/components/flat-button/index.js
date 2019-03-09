079c5fb123703a6b12e66938f250f8d01411747b
Marco Bruno <marco.bruno.br@gmail.com>
2019-03-09 18:47:22 +0000

    V1 039 07 Deixar o flat-button único

--- src/components/flat-button/index.js
+++ src/components/flat-button/index.js
@@ -1,12 +1,14 @@
 const flatButton = (function() {
   const module = {};
 
+  module._id = 0;
+
   module._style = active => {
     const $head = document.querySelector("head");
     const $style = document.createElement("style");
 
     $style.textContent = `
-      .flat-button {
+      .flat-button-${module._id} {
         background-color: ${active ? "#f25a70" : "#eae6da"};
         color: ${active ? "#fff" : "#fffcee"};
         font-size: 24px;
@@ -20,9 +22,10 @@ const flatButton = (function() {
   };
 
   module.render = (content = "", active = false) => {
+    module._id++;
     module._style(active);
 
-    return `<button class="flat-button">${content}</button>`;
+    return `<button class="flat-button-${module._id}">${content}</button>`;
   };
 
   return {
