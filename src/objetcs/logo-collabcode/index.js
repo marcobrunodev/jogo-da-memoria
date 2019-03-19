d69235f3b86b6f4173934bd3cdf134324cbcb0ce
Marco Bruno <marco.bruno.br@gmail.com>
2019-03-19 19:06:54 +0000

    V1 040 02 CSS no logo do Avatar

--- src/objetcs/logo-collabcode/index.js
+++ src/objetcs/logo-collabcode/index.js
@@ -1,7 +1,24 @@
 const logoCollabcode = (function() {
   const module = {};
 
-  module.render = () => `<img src="/img/icon-collabcode.png">`;
+  module._style = () => {
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+
+    $style.textContent = `
+      .logo-collabcode {
+        width: 175px;
+      }
+    `;
+
+    $head.insertAdjacentElement("beforeend", $style);
+  };
+
+  module.render = () => {
+    module._style();
+
+    return `<img class="logo-collabcode" src="/img/icon-collabcode.png">`;
+  };
 
   return {
     render: module.render
