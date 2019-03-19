8ffed1fef9090d187f73dcb08f6a82eb607d26af
Marco Bruno <marco.bruno.br@gmail.com>
2019-03-19 19:56:14 +0000

    V1 040 03 HTML do nosso título

---
+++ src/objetcs/title-collabcode/index.js
@@ -0,0 +1,9 @@
+const titleCollabcode = (function() {
+  const module = {};
+
+  module.render = () => `<h1>WELCOME!</h1>`;
+
+  return {
+    render: module.render
+  };
+})();
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -4,8 +4,10 @@
   const $loginButton = flatButton.render("Log in", false);
   const $signupButton = flatButton.render("Sign up", true);
   const $logoCollabcode = logoCollabcode.render();
+  const $titleCollabcode = titleCollabcode.render();
 
   $root.insertAdjacentHTML("beforeend", $loginButton);
   $root.insertAdjacentHTML("beforeend", $signupButton);
   $root.insertAdjacentHTML("beforeend", $logoCollabcode);
+  $root.insertAdjacentHTML("beforeend", $titleCollabcode);
 })();
