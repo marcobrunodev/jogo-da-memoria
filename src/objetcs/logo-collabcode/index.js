c5ea25d31b87c2b222466554e91e463d4b5c431c
Marco Bruno <marco.bruno.br@gmail.com>
2019-03-19 18:57:15 +0000

    V1 040 01 HTML do nosso logo avatar

---
+++ src/objetcs/logo-collabcode/index.js
@@ -0,0 +1,9 @@
+const logoCollabcode = (function() {
+  const module = {};
+
+  module.render = () => `<img src="/img/icon-collabcode.png">`;
+
+  return {
+    render: module.render
+  };
+})();
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -3,7 +3,9 @@
 
   const $loginButton = flatButton.render("Log in", false);
   const $signupButton = flatButton.render("Sign up", true);
+  const $logoCollabcode = logoCollabcode.render();
 
   $root.insertAdjacentHTML("beforeend", $loginButton);
   $root.insertAdjacentHTML("beforeend", $signupButton);
+  $root.insertAdjacentHTML("beforeend", $logoCollabcode);
 })();
