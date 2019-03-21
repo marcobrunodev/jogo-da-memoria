72c06c38ec8de372d9ad8f1c2b7674552454384e
Marco Bruno <marco.bruno.br@gmail.com>
2019-03-21 18:40:06 +0000

    V1 041 06 Dar vida para o input-collabcode

--- src/objetcs/input-collabcode/index.js
+++ src/objetcs/input-collabcode/index.js
@@ -1,7 +1,31 @@
 const inputCollabcode = (function() {
   const module = {};
 
-  module.render = () => `<input type="email" />`;
+  module._style = () => {
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+
+    $style.textContent = `
+      .input-collabcode {
+        display: block;
+        width: 100%;
+        color: #3a4042;
+        font-size: 18px;
+        font-weight: bold;
+        border-bottom: 2px solid rgba(58, 64, 66, 0.5);
+        padding-top: 12px;
+        padding-bottom: 12px;
+      }
+    `;
+
+    $head.insertAdjacentElement("beforeend", $style);
+  };
+
+  module.render = () => {
+    module._style();
+
+    return `<input class="input-collabcode" type="text" />`;
+  };
 
   return {
     render: module.render
