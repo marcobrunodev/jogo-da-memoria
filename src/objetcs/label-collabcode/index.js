77797a273d5540698fcaa8817794846aea6f41e7
Marco Bruno <marco.bruno.br@gmail.com>
2019-03-21 18:22:21 +0000

    V1 041 04 Dar vida para o label-collabcode

--- src/objetcs/label-collabcode/index.js
+++ src/objetcs/label-collabcode/index.js
@@ -1,7 +1,25 @@
 const labelCollabcode = (function() {
   const module = {};
 
-  module.render = content => `<label>${content}</label>`;
+  module._style = () => {
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+
+    $style.textContent = `
+      .label-collabcode {
+        color: #3a4042;
+        font-size: 16px;
+      }
+    `;
+
+    $head.insertAdjacentElement("beforeend", $style);
+  };
+
+  module.render = content => {
+    module._style();
+
+    return `<label class="label-collabcode">${content}</label>`;
+  };
 
   return {
     render: module.render
