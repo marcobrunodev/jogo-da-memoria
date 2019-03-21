e0464020d9cfe6377941373a0d57dc3afb306b48
Marco Bruno <marco.bruno.br@gmail.com>
2019-03-21 18:03:18 +0000

    V1 041 01 Posicionar texto dos botões

--- src/components/flat-button/index.js
+++ src/components/flat-button/index.js
@@ -9,11 +9,16 @@ const flatButton = (function() {
 
     $style.textContent = `
       .flat-button-${module._id} {
+        box-sizing: border-box;
+        display: inline-block;
+        text-decoration: none;
+        text-align: center;
         background-color: ${active ? "#f25a70" : "#eae6da"};
         color: ${active ? "#fff" : "#fffcee"};
         font-size: 24px;
         font-weight: bold;
         width: 50%;
+        padding-top: 60px;
         height: 176px;
         text-transform: uppercase;
       }
@@ -25,7 +30,7 @@ const flatButton = (function() {
     module._id++;
     module._style(active);
 
-    return `<button class="flat-button-${module._id}">${content}</button>`;
+    return `<a  href="#" class="flat-button-${module._id}">${content}</a>`;
   };
 
   return {
