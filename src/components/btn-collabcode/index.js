c7515172ca87b1c4690d716a44c92cf262570369
Marco Bruno <marco.bruno.br@gmail.com>
2019-03-22 18:25:40 +0000

    V1 042 11 CSS do no nosso btn-collabcode

--- src/components/btn-collabcode/index.js
+++ src/components/btn-collabcode/index.js
@@ -1,7 +1,31 @@
 const btnCollabcode = (function() {
   const module = {};
 
-  module.render = content => `<input type="submit" value=${content}>`;
+  module._style = () => {
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+
+    $style.textContent = `
+      .btn-collabcode {
+        height: 48px;
+        border-radius: 24px;
+        width: 100%;
+        color: white;
+        font-size: 14px;
+        text-transform: uppercase;
+        background-color: #f25a70;
+        cursor: pointer;
+      }
+    `;
+
+    $head.insertAdjacentElement("beforeend", $style);
+  };
+
+  module.render = content => {
+    module._style();
+
+    return `<input class="btn-collabcode" type="submit" value=${content}>`;
+  };
 
   return {
     render: module.render
