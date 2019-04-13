ffbdc511b38ff02d8eee0deb321803f17da82531
Marco Bruno <marco.bruno.br@gmail.com>
2019-04-13 14:47:43 +0000

    V1 043 06 Criar nosso form-login

---
+++ src/components/form-login/index.js
@@ -0,0 +1,51 @@
+const formLogin = (function() {
+  const module = {};
+
+  module._style = () => {
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+
+    $style.textContent = `
+      .form-signup {
+        padding: 0 35px 40px;
+      }
+    `;
+
+    $head.insertAdjacentElement("beforeend", $style);
+  };
+
+  module._children = () => {
+    const $usernameLabel = labelCollabcode.render("Username ou e-mail");
+    const $usernameInput = inputCollabcode.render({
+      placeholder: "marco.bruno.br@gmail.com"
+    });
+
+    const $passwordLabel = labelCollabcode.render("Password");
+    const $passwordInput = inputCollabcode.render({
+      placeholder: "*******",
+      type: "password"
+    });
+
+    const $btn = btnCollabcode.render("Login");
+
+    return `
+      ${$usernameLabel}
+      ${$usernameInput}
+
+      ${$passwordLabel}
+      ${$passwordInput}
+
+      ${$btn}
+    `;
+  };
+
+  module.render = () => {
+    module._style();
+
+    return `<form class="form-signup" action="" method="POST">${module._children()}</form>`;
+  };
+
+  return {
+    render: module.render
+  };
+})();
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -6,8 +6,10 @@
   const $logoCollabcode = logoCollabcode.render();
   const $titleCollabcode = titleCollabcode.render("Hello!");
   const $logoWrapper = logoWrapper.render($logoCollabcode, $titleCollabcode);
+  const $formLogin = formLogin.render();
 
   $root.insertAdjacentHTML("beforeend", $loginButton);
   $root.insertAdjacentHTML("beforeend", $signupButton);
   $root.insertAdjacentHTML("beforeend", $logoWrapper);
+  $root.insertAdjacentHTML("beforeend", $formLogin);
 })();
