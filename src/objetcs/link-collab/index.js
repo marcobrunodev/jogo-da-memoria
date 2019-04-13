d415c0d89087730c3d08ac4ae7fcaf0fd78f712a
Marco Bruno <marco.bruno.br@gmail.com>
2019-04-13 15:19:10 +0000

    V1 043 08 CSS do link-collab

--- src/objetcs/link-collab/index.js
+++ src/objetcs/link-collab/index.js
@@ -1,13 +1,31 @@
 const linkCollab = (function() {
   const module = {};
 
-  module._style = () => {};
+  module._style = () => {
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+
+    $style.textContent = `
+      .link-collab {
+        display: block;
+        text-decoration: none;
+        color: #3a4042;
+        font-size: 14px;
+        opacity: 0.7;
+        margin-top: 40px;
+        margin-bottom: 60px;
+        text-align: right;
+      }
+    `;
+
+    $head.insertAdjacentElement("beforeend", $style);
+  };
 
   module.render = ({ href, content }) => {
     module._style();
 
     return `
-      <a href="${href}">${content}</a>
+      <a class="link-collab" href="${href}">${content}</a>
     `;
   };
 
