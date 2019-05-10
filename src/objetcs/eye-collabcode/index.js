13d91226384b2aae872a30c7aeaefaa55370891c
Marco Bruno <marco.bruno.br@gmail.com>
2019-05-10 11:35:41 +0100

    Adicinou a imagem eye.png por CSS

--- src/objetcs/eye-collabcode/index.js
+++ src/objetcs/eye-collabcode/index.js
@@ -1,11 +1,32 @@
 const eyeCollabcode = (function() {
     const module = {};
 
-    module._style = () => {};
+    module._style = () => {
+        const $head = document.querySelector("head");
+        const $style = document.createElement("style");
 
-    module.render = () => `
-        <label>Mostrar senha</label>
-    `;
+        $style.textContent = `
+            .eye-collabcode {
+                background-image: url(/img/eye.png);
+                background-repeat: no-repeat;
+                background-position: center;
+                display: block;
+                text-indent: -9999px;
+                width: 24px;
+                height: 15px;
+            }
+        `
+
+        $head.insertAdjacentElement("beforeend", $style);
+    };
+
+    module.render = () => {
+        module._style()
+
+        return `
+            <label class="eye-collabcode">Mostrar senha</label>
+        `;
+    };
 
     return {
         render: module.render
