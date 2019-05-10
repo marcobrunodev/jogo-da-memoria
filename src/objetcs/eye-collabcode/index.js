ae52cab0079ed85d9c4cd7b973378d182d120d0f
Marco Bruno <marco.bruno.br@gmail.com>
2019-05-10 11:52:31 +0100

    Adicinamos o evento de click no eye-collabcode

--- src/objetcs/eye-collabcode/index.js
+++ src/objetcs/eye-collabcode/index.js
@@ -14,21 +14,29 @@ const eyeCollabcode = (function() {
                 text-indent: -9999px;
                 width: 24px;
                 height: 15px;
+                cursor: pointer;
             }
         `
 
         $head.insertAdjacentElement("beforeend", $style);
     };
 
+    module.handleClick = () => {
+        console.log("Ae!!!")
+    }
+
     module.render = () => {
         module._style()
 
         return `
-            <label class="eye-collabcode">Mostrar senha</label>
+            <label 
+                class="eye-collabcode"
+                onClick="eyeCollabcode.handleClick()">Mostrar senha</label>
         `;
     };
 
     return {
-        render: module.render
+        render: module.render,
+        handleClick: module.handleClick
     }
 })();
